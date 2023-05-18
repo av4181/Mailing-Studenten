64507fd59f5b3a29b19bd4a9b4d147c5b3aa427c
Steven Symons <steven.symons@student.kdg.be>
2023-05-18 18:49:57 +0200

    fix sorting by grades

--- MailingStudenten/src/studenten/view/aanmakenmails/table/AanmakenMailsTablePresenter.java
+++ MailingStudenten/src/studenten/view/aanmakenmails/table/AanmakenMailsTablePresenter.java
@@ -11,6 +11,11 @@ import javafx.util.Callback;
 import studenten.model.PeriodeResultaat;
 import studenten.model.PeriodeResultaten;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Comparator;
+import java.util.Locale;
+
 public class AanmakenMailsTablePresenter {
     private PeriodeResultaten model;
     private AanmakenMailsTableView view;
@@ -48,6 +53,28 @@ public class AanmakenMailsTablePresenter {
                 return new SimpleStringProperty(String.format("%.2f", periodeResultaatStringCellDataFeatures.getValue().berekenGemiddelde()));
             }
         });
+        this.view.getPuntKolom().setComparator(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                try {
+                    NumberFormat numberFormat = NumberFormat.getInstance(Locale.GERMANY);
+
+                    double number1 = numberFormat.parse(o1).doubleValue();
+                    double number2 = numberFormat.parse(o2).doubleValue();
+
+                    if (number1 == number2) {
+                        return 0;
+                    }
+                    if (number1 < number2) {
+                        return -1;
+                    }
+                    return 1;
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
         this.view.getPeriodeKolom().setCellValueFactory(new Callback<TableColumn.CellDataFeatures<PeriodeResultaat, String>, ObservableValue<String>>() {
             @Override
             public ObservableValue<String> call(TableColumn.CellDataFeatures<PeriodeResultaat, String> periodeResultaatStringCellDataFeatures) {
